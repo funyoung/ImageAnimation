99a7a46656e910f6057b20cd9c3c55b23832cdfb
YangFeng <e13310@gmail.com>
2013-09-01 02:46:59 +0800

    On branch dev-qcwx-dbg-sliding  Changes to be committed: 	modified:   res/layout/text_fragment.xml 	modified:   src/com/funyoung/quickrepair/fragment/TextFragment.java

--- src/com/funyoung/quickrepair/fragment/TextFragment.java
+++ src/com/funyoung/quickrepair/fragment/TextFragment.java
@@ -8,20 +8,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 import com.funyoung.qcwx.R;
+import com.funyoung.quickrepair.MainActivity;
 import com.funyoung.view.OnTextFragmentAnimationEndListener;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TextFragment extends Fragment {
+    public static final String ARG_CAT_ID = "ARG_CAT_ID";
+    public static final String ARG_CAT_LABEL = "ARG_CAT_LABEL";
 
     View.OnClickListener clickListener;
     OnTextFragmentAnimationEndListener mListener;
 
+    private View rootView;
+    private ArrayList<String> mSubCategory = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    private int mMainId = 0;
+    private String mMainLabel;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.text_fragment, container, false);
-        view.setOnClickListener(clickListener);
-        return view;
+        rootView = inflater.inflate(R.layout.text_fragment, container, false);
+        rootView.setOnClickListener(clickListener);
+
+        Bundle bundle = getArguments();
+        if (null != bundle) {
+            mMainId = bundle.getInt(ARG_CAT_ID, 0);
+            mMainLabel = bundle.getString(ARG_CAT_LABEL, "NA");
+        }
+        initSubCategory();
+        return rootView;
     }
 
     public void setClickListener(View.OnClickListener clickListener) {
@@ -48,4 +69,32 @@ public class TextFragment extends Fragment {
     {
         mListener = listener;
     }
+
+    private void initSubCategory() {
+        String[] subLabels = getResources().getStringArray(MainActivity.subLabelArray[mMainId]);
+        mSubCategory.clear();
+        mSubCategory.addAll(Arrays.asList(subLabels));
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.ptr_gridview_sub);
+        adapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.gridview_item_category_sub,
+                mSubCategory);
+
+//        SimpleAdapter adapter = new SimpleAdapter(getActivity(),
+//                itemData,
+//                R.layout.gridview_item_category,
+//                new String[] { "img", "label" },
+//                new int[] { R.id.img, R.id.label });
+
+        gridView.setAdapter(adapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                int[] subIds = getResources().getIntArray(MainActivity.subIdArray[mMainId]);
+                ((MainActivity)getActivity()).startPost(mMainId + 1, subIds[i],
+                        mMainLabel, mSubCategory.get(i));
+            }
+        });
+    }
+
 }
